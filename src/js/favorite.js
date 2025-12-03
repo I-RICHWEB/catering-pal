fceef4a320f704eb10571c1bdf9238560d7bbf78
I-RICHWEB <ize20002@byui.edu>
2025-12-03 14:32:04 +0100

    completed the html structure for the site.

---
+++ src/js/favorite.js
@@ -0,0 +1,10 @@
+/* ******************************************
+ ** Importing all the neccessaries tools needed
+ ** *************************************** */
+import { loadHeaderFooter } from "./utility.mjs";
+
+/* ******************************************
+ ** Calling the loadHeaderFooter function to
+ ** action.
+ ** *************************************** */
+loadHeaderFooter();
--- src/js/utility.mjs
+++ src/js/utility.mjs
@@ -2,7 +2,19 @@
  ** This function is going to be use for the
  ** hero images slide show.
  ** *************************************** */
-export function heroSlideShow(images, element) {
+export function heroSlideShow() {
+  const images = [
+    "barbecue.jpg",
+    "Fried-salmon.jpg",
+    "fruit-juice.jpg",
+    "fruit-splash.jpg",
+    "grilled-chicken.jpg",
+    "served-food.jpg",
+    "set-of-foods.jpg",
+    "Sushi.jpg",
+  ];
+  const element = document.getElementById("heros");
+
   const path = "./images/heros/";
   let index = 0;
 
@@ -16,3 +28,43 @@ export function heroSlideShow(images, element) {
   updateImageSrc();
   setInterval(updateImageSrc, 5000);
 }
+
+/* ******************************************
+ ** This function is going to load the header
+ ** and footer from the partials folder.
+ ** *************************************** */
+export async function loadTemplate(path) {
+  const template = (await fetch(path)).text();
+  return template;
+}
+
+/* ******************************************
+ ** This function is going to render the HTML
+ ** into the document that is calling it.
+ ** *************************************** */
+export function renderTemplate(htmlTemplate, parentElement, data, callback) {
+  parentElement.innerHTML = htmlTemplate;
+
+  if (callback) {
+    callback(data);
+  }
+}
+
+/* ******************************************
+ ** This function is going to load the header
+ ** and footer from the partials folder.
+ ** *************************************** */
+export async function loadHeaderFooter() {
+  const headerData = await loadTemplate("../partials/header.html");
+  const footerData = await loadTemplate("../partials/footer.html");
+
+  const headerElement = document.querySelector(".headings");
+  const footerElement = document.querySelector(".footer");
+  renderTemplate(headerData, headerElement);
+  renderTemplate(footerData, footerElement);
+}
+
+/* ******************************************
+ ** This function is going to load the header
+ ** and footer from the partials folder.
+ ** *************************************** */
